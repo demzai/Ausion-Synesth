611df6b3b2be171c629be3680087636a4bf9bcb7
demzai <demetrius.zaibo@students.plymouth.ac.uk>
2019-02-10 14:08:44 +0000

    Relegated code to own function
    Enables repeat calls -> more suitable for video processing
    Converted the histograms from arrays to vectors for easier manipulation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/features2d.hpp>
@@ -14,12 +15,17 @@ void fft(cv::Mat &input, cv::Mat &output);
 void getIndexes(int num_channels, int num_hist_bins,
                 int img_rows, int img_cols,
                 int x, int y, int *index);
+void imageToSound(bool is_single_channel, Mat &img_in, Mat &img_out,
+                  int num_channels, int num_hist_bins,
+                  vector<vector<double> > &histograms);
 
 /// CONSTANTS ///
 const double pi = 3.141592653589;
 
 int main()
 {
+    //////////////////////
+    /// Initialisation ///
     // Define meta-parameters
     const bool is_single_channel = true;
     const int num_channels = 4;
@@ -28,67 +34,25 @@ int main()
             "C:\\Users\\Student\\Desktop\\Youtube\\Pics\\bilat_screenshot_04.08.2017.png";
     // "C:\\Users\\Student\\Desktop\\Personal\\Resume\\Face Pics\\DSC_0187.JPG"
 
-    ////////////////////////////////////////
-    /// Convert the image into FFT space ///
+    // Initialise the image source
     Mat in = imread(img_locale);
     Mat out;
     if( in.empty())
     {return -1;}
 
-    // Single channel
-    if(is_single_channel == true)
+    // Initialise the audio results
+    vector<vector<double>> histograms;
     {
-        cvtColor(in, in, CV_BGR2GRAY);
-        fft(in, out);
-    }
-    // Multi-channel
-    else
-    {
-        Mat time[3], freq[3];
-        cv::split(in, time);
-        for(int i = 0; i < 3; i++)
-        {fft(time[i], freq[i]);}
-        cv::merge(freq, 3, out);
+        vector<double> temp;
+        temp.resize(num_hist_bins, 0.0);
+        histograms.resize(num_channels, temp);
     }
 
-    ///////////////////////////////
-    /// Acquire the audio fft's ///
-    double histograms[num_channels][num_hist_bins];
 
-    // Initialise the histograms to zero
-    for(int i = 0; i < num_channels; i++)
-    {
-        for(int j = 0; j < num_hist_bins; j++)
-        {histograms[i][j] = 0;}
-    }
+    ////////////////////
+    /// Main Program ///
+    imageToSound(is_single_channel, in, out, num_channels, num_hist_bins, histograms);
 
-    // Create matrices to provide fast indexing
-    Mat index_channel = Mat::zeros(in.rows, in.cols, CV_32SC1);
-    Mat index_bin = Mat::zeros(in.rows, in.cols, CV_32SC1);
-    for (int i = 0; i < in.rows; i++)
-    {
-        int* pix_chan = index_channel.ptr<int>(i);
-        int* pix_bin = index_bin.ptr<int>(i);
-        int index[2];
-        for (int j = 0; j < in.cols; ++j)
-        {
-            getIndexes(num_channels, num_hist_bins, in.rows, in.cols, j, i, index);
-            pix_chan[j] = index[0];
-            pix_bin[j] = index[1];
-        }
-    }
-
-    // Convert the FFT'd image into audio FFT's
-    for (int i = 0; i < in.rows; i++)
-    {
-        int* pix_chan = index_channel.ptr<int>(i);
-        int* pix_bin = index_bin.ptr<int>(i);
-        float* pix_fft = out.ptr<float>(i);
-        for (int j = 0; j < in.cols; ++j)
-        {
-            histograms[pix_chan[j]][pix_bin[j]] += pix_fft[j];
-        }
-    }
 
     ////////////////////////
     /// Show the results ///
@@ -119,7 +83,7 @@ int main()
 }
 
 
-
+/// Convert an input greyscale image into the frequency domain
 void fft(cv::Mat &input, cv::Mat &output)
 {
     // Courtesy of https://docs.opencv.org/3.4/d8/d01/tutorial_discrete_fourier_transform.html
@@ -171,6 +135,7 @@ void fft(cv::Mat &input, cv::Mat &output)
     magI.copyTo(output);
 }
 
+/// For a given x,y coordinate, which audio channel and histogram bin does it belong to?
 void getIndexes(int num_channels, int num_hist_bins,
                 int img_rows, int img_cols,
                 int x, int y, int *index)
@@ -198,7 +163,68 @@ void getIndexes(int num_channels, int num_hist_bins,
     return;
 }
 
-void imageToSound();
+/// For a given image, convert into a set of frequency responses for a set of speakers
+void imageToSound(bool is_single_channel, Mat &img_in, Mat &img_out,
+                  int num_channels, int num_hist_bins,
+                  vector<vector<double>> &histograms)
+{
+    ////////////////////////////////////////
+    /// Convert the image into FFT space ///
+    // Single channel
+    if(is_single_channel == true)
+    {
+        cvtColor(img_in, img_in, CV_BGR2GRAY);
+        fft(img_in, img_out);
+    }
+    // Multi-channel
+    else
+    {
+        Mat time[3], freq[3];
+        cv::split(img_in, time);
+        for(int i = 0; i < 3; i++)
+        {fft(time[i], freq[i]);}
+        cv::merge(freq, 3, img_out);
+    }
+
+    ///////////////////////////////
+    /// Acquire the audio fft's ///
+    // Initialise the histograms to zero
+    for(int i = 0; i < num_channels; i++)
+    {
+        for(int j = 0; j < num_hist_bins; j++)
+        {histograms[i][j] = 0;}
+    }
+
+    // Create matrices to provide fast indexing
+    Mat index_channel = Mat::zeros(img_in.rows, img_in.cols, CV_32SC1);
+    Mat index_bin = Mat::zeros(img_in.rows, img_in.cols, CV_32SC1);
+    for (int i = 0; i < img_in.rows; i++)
+    {
+        int* pix_chan = index_channel.ptr<int>(i);
+        int* pix_bin = index_bin.ptr<int>(i);
+        int index[2];
+        for (int j = 0; j < img_in.cols; ++j)
+        {
+            getIndexes(num_channels, num_hist_bins, img_in.rows, img_in.cols, j, i, index);
+            pix_chan[j] = index[0];
+            pix_bin[j] = index[1];
+        }
+    }
+
+    // Convert the FFT'd image into audio FFT's
+    for (int i = 0; i < img_in.rows; i++)
+    {
+        int* pix_chan = index_channel.ptr<int>(i);
+        int* pix_bin = index_bin.ptr<int>(i);
+        float* pix_fft = img_out.ptr<float>(i);
+        for (int j = 0; j < img_in.cols; ++j)
+        {
+            histograms[pix_chan[j]][pix_bin[j]] += pix_fft[j];
+        }
+    }
+
+    return;
+}
 
 
 
